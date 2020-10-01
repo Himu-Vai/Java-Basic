eb13052efb1848e590c5b98757d3575af4829618
Himu-Vai <71009349+Himu-Vai@users.noreply.github.com>
2020-10-01 10:36:07 +0600

    Add files via upload

---
+++ Basic/src/basic/Basic.java
@@ -0,0 +1,12 @@
+
+package basic;
+
+
+public class Basic {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
---
+++ Basic/src/basic/ShowValues.java
@@ -0,0 +1,20 @@
+
+package basic;
+
+
+public class ShowValues {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println("The value is : "+a);
+        float b=(float) 23.56;
+        System.out.println("The value is: "+b);
+        double c=345.45;
+        System.out.println("The value is "+c);
+        char d='h';
+        System.out.println("The value is:"+d);
+        
+        
+        
+    }
+    
+}
